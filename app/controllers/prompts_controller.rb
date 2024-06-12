bf782e37f8e603bf7f908dd1e4bb8f14078c2ba5
takuro <talangxiaotian6@gmail.com>
2024-06-13 03:37:07 +0900

    コントローラーとビューの設定

---
+++ app/controllers/prompts_controller.rb
@@ -0,0 +1,50 @@
+class PromptsController < ApplicationController
+  def index
+    @prompts = Prompt.all
+  end
+
+  def show
+    @prompt = Prompt.find(params[:id])
+  end
+
+  def new
+    @prompt = Prompt.new
+  end
+
+  def create
+    @prompt = Prompt.new(prompt_params)
+
+    if @prompt.save
+      redirect_to @prompt
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @prompt = Prompt.find(params[:id])
+  end
+
+  def update
+    @prompt = Prompt.find(params[:id])
+
+    if @prompt.update(prompt_params)
+      redirect_to @prompt
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @prompt = Prompt.find(params[:id])
+    @prompt.destroy
+
+    redirect_to prompts_path, status: :see_other
+  end
+
+  private
+
+  def prompt_params
+    params.require(:prompt).permit(:title, :text)
+  end
+end
